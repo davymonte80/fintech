c0afa2516dee386ff7dbc46023131db5ffb9477f
David Monte <David.Monte@healthyu2000.com>
2024-08-23 10:04:30 +0300

    update login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,10 +15,7 @@ export {
   ErrorBoundary,
 } from 'expo-router';
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
+
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -53,7 +50,7 @@ function RootLayoutNav() {
   return (
     
       <Stack>
-       
+    <Stack.Screen name= "index" options={ {headerShown:false}} />
       </Stack>
 
   );
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,8 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import { useAssets } from 'expo-asset'
 
 const Page = () => {
+  const {assets} = useAssets ([ require ('@/assets/fonts/SpaceMono-Regular.ttf')])
   return (
     <View>
       <Text>Home</Text>
