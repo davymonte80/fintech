d8fc74776e4cf33b2c1266ede52204053d09039c
David Monte <David.Monte@healthyu2000.com>
2024-09-12 16:31:48 +0300

    update modules

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native'
 import { useAssets } from 'expo-asset'
 
 const Page = () => {
-  const {assets} = useAssets ([ require ('@/assets/fonts/SpaceMono-Regular.ttf')])
+  const [assets] = useAssets ([ require ('@/assets/fonts/SpaceMono-Regular.ttf')])
   return (
     <View>
       <Text>Home</Text>
